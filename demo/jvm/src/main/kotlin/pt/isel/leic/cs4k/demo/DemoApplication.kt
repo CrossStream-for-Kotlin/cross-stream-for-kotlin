0c12bc3829ba7428bb9c316ba8861a60ed28f051
João Guilherme Bonacho <A49437@alunos.isel.pt>
2024-06-01 10:59:51 +0100

    updates demo

--- demo/jvm/src/main/kotlin/pt/isel/leic/cs4k/demo/DemoApplication.kt
+++ demo/jvm/src/main/kotlin/pt/isel/leic/cs4k/demo/DemoApplication.kt
@@ -4,6 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import pt.isel.leic.cs4k.independentBroker.BrokerIndependent
+import pt.isel.leic.cs4k.independentBroker.serviceDiscovery.config.DNSServiceDiscoveryConfig
+import pt.isel.leic.cs4k.independentBroker.serviceDiscovery.config.MulticastServiceDiscoveryConfig
 import pt.isel.leic.cs4k.postgreSQL.BrokerPostgreSQL
 import pt.isel.leic.cs4k.redis.BrokerRedis
 import pt.isel.leic.cs4k.redis.RedisNode
@@ -11,10 +13,14 @@ import pt.isel.leic.cs4k.redis.RedisNode
 @SpringBootApplication
 class DemoApplication {
 
+    // Option 1
+
     @Bean
     fun brokerPostgreSQL() = BrokerPostgreSQL(
         Environment.getPostgreSQLDbUrl()
     )
+
+    // Option 2
 
     // @Bean
     fun brokerRedis() = BrokerRedis(
@@ -24,10 +30,22 @@ class DemoApplication {
         )
     )
 
+    // Option 3
+
+    // TODO(Add RabbitMQ)
+
+    // Option 4
+
     // @Bean
-    fun brokerIndependent() = BrokerIndependent(
-        Environment.getHostname()
-        /* Environment.getServiceName() */
+    fun brokerIndependentMulticastServiceDiscovery() = BrokerIndependent(
+        Environment.getHostname(),
+        MulticastServiceDiscoveryConfig(Environment.getHostname())
+    )
+
+    // @Bean
+    fun brokerIndependentDNSServiceDiscovery() = BrokerIndependent(
+        Environment.getHostname(),
+        DNSServiceDiscoveryConfig(Environment.getHostname(), Environment.getServiceName())
     )
 }
 
